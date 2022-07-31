bf399b7b4b993aea3360050111fb3ff14b2c1b50
nataliia-stepanenko <natalyss.contact@gmail.com>
2022-08-01 00:42:58 +0300

    Added index.html and script folder

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+let userName = prompt("Как тебя зовут?");
+alert("Привет, " + userName + "!");
+
+let a = prompt("Напиши число a", 12);
+let b = prompt("А теперь напиши число b", 3);
+alert("Смотри, чего могу:");
+alert("a + b = " + (+a + +b));
+alert("a - b = " + (a - b));
+alert("a * b = " + (a * b));
+alert("a / b = " + (a / b));
